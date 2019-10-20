faa5842065d6446e5aa6388037e621a8a565adb6
saloev <saloev.saadi@yandex.ru>
2019-10-20 20:39:10 +0300

    feat: add getTimeForEducation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const HOURS_TO_MASTER_JS = 800;
+const HOURS_TO_BASIC_PROGRAMMING = 500;
+
 /**
  * @param preferences - target student focus
  * @param knowsProgramming - if student can do programming and know basics
@@ -5,10 +8,15 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = "family",
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  if (knowsProgramming) {
+    return Math.ceil(HOURS_TO_MASTER_JS / config[focus]);
+  }
+
+  return Math.ceil(
+    (HOURS_TO_MASTER_JS + HOURS_TO_BASIC_PROGRAMMING) / config[focus]
+  );
+};
